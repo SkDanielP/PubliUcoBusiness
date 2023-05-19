3ebd4a728f7a4a3f9aea2456cca213698a61b134
SkDanielP <113185316+SkDanielP@users.noreply.github.com>
2023-05-19 18:54:12 -0500

    commit clase

--- PubliUcoBusiness/src/co/edu/uco/business/businessimpl/EstadoTipoRelacionInstitucionBusinessImpl.java
+++ PubliUcoBusiness/src/co/edu/uco/business/businessimpl/EstadoTipoRelacionInstitucionBusinessImpl.java
@@ -1,10 +1,13 @@
 package co.edu.uco.business.businessimpl;
 
 import java.util.List;
+import java.util.UUID;
 
 import co.edu.uco.business.business.EstadoTipoRelacionInstitucionBusiness;
 import co.edu.uco.business.domain.EstadoTipoRelacionInstitucionDomain;
 import co.edu.uco.business.domain.assembler.concrete.EstadoTipoRelacionInstitucionAssembler;
+import co.edu.uco.publiuco.crosscutting.exception.PubliucoBussinesException;
+import co.edu.uco.publiuco.crosscutting.utils.UtilUUID;
 import co.edu.uco.publiuco.data.dao.factory.DAOFactory;
 import co.edu.uco.publiuco.entities.EstadoTipoRelacionInstitucionEntity;
 
@@ -18,6 +21,25 @@ public final class EstadoTipoRelacionInstitucionBusinessImpl implements EstadoTi
 
 	@Override
 	public final void register(final EstadoTipoRelacionInstitucionDomain domain) {
+		
+		UUID identificador;
+		EstadoTipoRelacionInstitucionEntity entityTmp;
+		List<EstadoTipoRelacionInstitucionEntity> result;
+		
+		do {
+			identificador = UtilUUID.generateNewUUID();
+			entityTmp = new EstadoTipoRelacionInstitucionEntity(identificador);
+			result = daoFactory.getEstadoTipoRelacionInstitucionDAO().read(entityTmp);
+		} while (!result.isEmpty());
+
+		entityTmp = EstadoTipoRelacionInstitucionEntity.createWithNombre(domain.getNombre());
+		result = daoFactory.getEstadoTipoRelacionInstitucionDAO().read(entityTmp);
+		
+		if(!result.isEmpty()) {
+			throw PubliucoBussinesException.create("El estado de tipo relacion institucion que intenta crear ya existe, Por favor verifique e intente nuevamente");
+		}
+		
+		
 		final EstadoTipoRelacionInstitucionEntity entity = EstadoTipoRelacionInstitucionAssembler.getInstance().toEntityFromDomain(domain);
 		daoFactory.getEstadoTipoRelacionInstitucionDAO().create(entity);
 		
